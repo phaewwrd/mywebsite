b4fa8b9761b36f18ba6dfa57ab8174812b86705f
phaewphanwrd <phaewphan.wrd@gmail.com>
2025-05-27 13:37:09 +0700

    fixed: layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutHeader from "@/components/header/LayoutHeader";
-import {Stars} from "@/components/background/Stars";
-import {Leaf} from "@/components/background/Leaf";
+import Stars from "@/components/background/Stars";
+import Leaf from "@/components/background/Leaf";
 import Header from "@/components/header/Desktop";
 import Footer from "@/components/Footer";
 
