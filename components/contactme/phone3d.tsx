b15556c2ee575b0f7c90e18ee5a047e8b201ca43
phaewphanwrd <phaewphan.wrd@gmail.com>
2025-05-28 11:28:41 +0700

    edited: phone3d.tsx

--- components/contactme/phone3d.tsx
+++ components/contactme/phone3d.tsx
@@ -6,7 +6,7 @@ export default function Phone3D() {
        <div className='animate-marquee-phone transition-all'>
         <Image src="/phone.png" alt="phone" width={300} height={500} className="w-60 xl:w-80" />
        </div>
-        <Image src="/phone-bg.png" alt="phone-bg" width={250} height={500} className="absolute xl:w-80 w-60 bottom-80 md:right-60 xl:right-60 2xl:right-130 -z-10" />
+        <Image src="/phone-bgcopy.png" alt="phone-bg" width={250} height={500} className="absolute xl:w-80 w-60 bottom-80 md:right-60 xl:right-60 2xl:right-130 -z-10" />
         
     </div>
   )
